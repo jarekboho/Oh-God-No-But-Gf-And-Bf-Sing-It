beb0dcfc64d2dbe0c0f37e00db253323ba3006bb
jarekboho <123318901+jarekboho@users.noreply.github.com>
2025-04-28 22:45:54 +0300

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -195,6 +195,24 @@ class Character extends FlxSprite
 				addOffset("singDOWN", 139, -171);
 
 				playAnim('idle');
+
+			case 'dad':
+				// DAD ANIMATION LOADING CODE
+				tex = Paths.getSparrowAtlas('DADDY_DEAREST','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'Dad idle dance', 24);
+				animation.addByPrefix('singUP', 'Dad Sing Note UP', 24);
+				animation.addByPrefix('singRIGHT', 'Dad Sing Note RIGHT', 24);
+				animation.addByPrefix('singDOWN', 'Dad Sing Note DOWN', 24);
+				animation.addByPrefix('singLEFT', 'Dad Sing Note LEFT', 24);
+
+				addOffset('idle');
+				addOffset("singUP", -6, 50);
+				addOffset("singRIGHT", 0, 27);
+				addOffset("singLEFT", -10, 10);
+				addOffset("singDOWN", 0, -30);
+
+				playAnim('idle');
 		}
 
 		dance();
